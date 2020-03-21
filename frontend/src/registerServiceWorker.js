3c4dcba9af891d3983a4af9ae1e3deeb2977dae1
Jonathan <40319156+joni1802@users.noreply.github.com>
2020-03-21 16:57:07 +0100

    modified update toast

--- frontend/src/registerServiceWorker.js
+++ frontend/src/registerServiceWorker.js
@@ -1,39 +1,60 @@
 /* eslint-disable no-console */
 
-import { register } from 'register-service-worker'
-import Vue from 'vue'
+import {register} from "register-service-worker";
+import Vue from "vue";
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   register(`${process.env.BASE_URL}service-worker.js`, {
-    ready () {
+    ready() {
       console.log(
-        'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB'
-      )
+        "App is being served from cache by a service worker.\n" +
+          "For more details, visit https://goo.gl/AFskqB"
+      );
     },
-    registered () {
-      console.log('Service worker has been registered.')
+    registered() {
+      console.log("Service worker has been registered.");
     },
-    cached () {
-      console.log('Content has been cached for offline use.')
+    cached() {
+      console.log("Content has been cached for offline use.");
     },
-    updatefound () {
-      console.log('New content is downloading.')
+    updatefound() {
+      console.log("New content is downloading.");
     },
-    updated () {
-      console.log('New content is available; please refresh.')
+    updated() {
+      console.log("New content is available; please refresh.");
 
-      Vue.prototype.$toast('NEW VERSION AVAILABLE 🦄 RELOAD PAGE', {
-        icon: 'mdi-alert-decagram',
-        color: '#ec407a',
+      // https://vuejs.org/v2/guide/render-function.html#The-Data-Object-In-Depth
+      Vue.prototype.$toast(null, {
+        icon: "mdi-alert-decagram",
+        color: "#ec407a",
         timeout: 0,
-      })
+        slot: [
+          "New Version Available",
+          this.$createElement(
+            "v-btn",
+            {
+              on: {
+                click: () => {
+                  localStorage.clear();
+                  location.reload();
+                }
+              },
+              attrs: {
+                color: "purple"
+              }
+            },
+            ["Update"]
+          )
+        ]
+      });
     },
-    offline () {
-      console.log('No internet connection found. App is running in offline mode.')
+    offline() {
+      console.log(
+        "No internet connection found. App is running in offline mode."
+      );
     },
-    error (error) {
-      console.error('Error during service worker registration:', error)
+    error(error) {
+      console.error("Error during service worker registration:", error);
     }
-  })
+  });
 }
