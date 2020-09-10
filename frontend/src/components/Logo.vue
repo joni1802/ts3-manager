84fef7914ef25038f5107606fc0d45381adefff3
Jonathan <40319156+joni1802@users.noreply.github.com>
2020-09-10 14:13:56 +0200

    decreased logo width

--- frontend/src/components/Logo.vue
+++ frontend/src/components/Logo.vue
@@ -1,6 +1,6 @@
 <template lang="html">
   <v-list-item two-line>
-    <v-list-item-avatar tile width="250">
+    <v-list-item-avatar tile width="200">
       <img src="@/assets/ts3_manager_text_dark.svg" />
     </v-list-item-avatar>
   </v-list-item>
@@ -8,6 +8,6 @@
 
 <script>
 export default {
-  
+
 }
 </script>
