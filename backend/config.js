5aa963feb9b5e526e685c57fa57fc107f26b7620
Jonathan <jonathan@francke.me>
2020-11-07 15:55:56 +0100

    fixed parsed whitelist

--- backend/config.js
+++ backend/config.js
@@ -8,12 +8,12 @@ program.option("-w, --whitelist <value>", "comma separated ip list of TeamSpeak 
 program.parse(process.argv)
 
 function parseWhitelist(value) {
-  return value.split(",")
+  return value ? value.split(",") : false
 }
 
 // process order of the parameters: command line > environment variable > default value
 module.exports = {
   port: program.port ? program.port : process.env.PORT || 3000,
   secret: program.secret ? program.secret : process.env.JWT_SECRET || crypto.randomBytes(256).toString("base64"),
-  whitelist: program.whitelist ? program.whitelist : process.env.WHITELIST || [],
+  whitelist: program.whitelist ? program.whitelist :  parseWhitelist(process.env.WHITELIST) || [],
 }
