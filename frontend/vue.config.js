fc363eb5a6d03e64609bab6852a67d253bbf8b92
Jonathan <jonathan@francke.me>
2021-04-20 22:48:37 +0200

    Fixed Logo #30

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -19,9 +19,9 @@ module.exports = {
     },
     iconPaths: {
       favicon32: 'img/icons/favicon.png',
-      appleTouchIcon: 'img/icons/ts3_manager_new.png',
-      maskIcon: 'img/icons/ts3_manager_new.svg',
-      msTileImage: 'img/icons/ts3_manager_new.png'
+      appleTouchIcon: 'img/icons/ts3_manager.png',
+      maskIcon: 'img/icons/ts3_manager.svg',
+      msTileImage: 'img/icons/ts3_manager.png'
     }
   }
 }
