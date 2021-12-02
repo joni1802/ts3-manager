0aa924d76358a7ad931002534452216dfa621d4d
Jonathan <jonathan@francke.me>
2021-12-02 01:24:15 +0100

    wip: update route when switching between servers

--- packages/ui/src/components/Server.vue
+++ packages/ui/src/components/Server.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <h1>Server {{ serverId }}</h1>
     <router-view v-if="joined"></router-view>
   </div>
 </template>
@@ -11,6 +10,7 @@ export default {
     return {
       serverId: Number(this.$route.params.sid),
       joined: false,
+      drawer: null,
     };
   },
   methods: {
@@ -19,12 +19,29 @@ export default {
 
       this.$store.dispatch("saveServerId", this.serverId);
     },
+    getServerInfo() {
+      return this.$TeamSpeak.execute("serverinfo").then((res) => res[0]);
+    },
     registerServerNotifications() {
       return this.$TeamSpeak.registerAllEvents();
+    },
+    async setServerHostBanner() {
+      let { virtualserver_hostbanner_gfx_url: hostBannerUrl } =
+        await this.getServerInfo();
+
+      this.$store.dispatch("setServerHostBanner", hostBannerUrl);
+    },
+    async setServerList() {
+      let serverList = await this.$TeamSpeak.execute("serverlist");
+
+      this.$store.commit("setServerList", serverList);
     },
   },
   async created() {
     try {
+      // to-do: check selected server id with whoami and set whoami in vuex
+      await this.setServerList();
+
       await this.useServer();
 
       await this.registerServerNotifications();
@@ -37,6 +54,35 @@ export default {
     } catch (err) {
       this.$toast.error(err.message);
     }
+  },
+  async beforeRouteUpdate(to, from, next) {
+    try {
+      if (from.params.sid !== to.params.sid) {
+        this.joined = false;
+
+        this.serverId = to.params.sid;
+
+        await this.useServer();
+
+        await this.registerServerNotifications();
+
+        this.joined = true;
+
+        if (to.name === "server") {
+          next({
+            name: "dashboard",
+            replace: true,
+            params: { sid: this.serverId },
+          });
+        }
+
+        next();
+      }
+    } catch (err) {
+      this.$toast.error(err.message);
+    }
+
+    next();
   },
 };
 </script>
