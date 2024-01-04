692306cdc33ff4a6cf9e8cd6ae5aa0395e85c65b
Jonathan <jonathan@francke.me>
2024-01-04 20:40:42 +0100

    added workaround for legacy openssl issue

--- packages/ui/vue.config.js
+++ packages/ui/vue.config.js
@@ -1,27 +1,44 @@
+const crypto = require("crypto");
+
+/**
+ * @link https://stackoverflow.com/a/72219174
+ * The MD4 algorithm is not available anymore in Node.js 17+ (because of library SSL 3).
+ * In that case, silently replace MD4 by the MD5 algorithm.
+ */
+try {
+  crypto.createHash("md4");
+} catch (e) {
+  console.warn('Crypto "MD4" is not supported anymore by this Node.js version');
+  const origCreateHash = crypto.createHash;
+  crypto.createHash = (alg, opts) => {
+    return origCreateHash(alg === "md4" ? "md5" : alg, opts);
+  };
+}
+
 module.exports = {
   lintOnSave: undefined,
   runtimeCompiler: true,
 
   pwa: {
-    name: 'TS3 Manager',
-    themeColor: '#FAFAFA',
-    msTileColor: '#FFFFFF',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-    manifestPath: 'manifest.json',
+    name: "TS3 Manager",
+    themeColor: "#FAFAFA",
+    msTileColor: "#FFFFFF",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+    manifestPath: "manifest.json",
     workboxOptions: {
-      navigateFallback: 'index.html',
+      navigateFallback: "index.html",
       // Ignore api calls because this routes are handled on the server side (backend).
       navigateFallbackBlacklist: [/\/api\/.*/],
       // To make update on refresh available.
       // See https://stackoverflow.com/questions/54145735/vue-pwa-not-getting-new-content-after-refresh
-      skipWaiting: true
+      skipWaiting: true,
     },
     iconPaths: {
-      favicon32: 'img/icons/favicon.png',
-      appleTouchIcon: 'img/icons/ts3_manager.png',
-      maskIcon: 'img/icons/ts3_manager.svg',
-      msTileImage: 'img/icons/ts3_manager.png'
-    }
-  }
-}
+      favicon32: "img/icons/favicon.png",
+      appleTouchIcon: "img/icons/ts3_manager.png",
+      maskIcon: "img/icons/ts3_manager.svg",
+      msTileImage: "img/icons/ts3_manager.png",
+    },
+  },
+};
