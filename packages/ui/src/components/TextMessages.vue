e5f3831c9275090c22e1b5b8cf79de1f7b13ae55
Jonathan <jonathan@francke.me>
2026-03-07 13:37:05 +0100

    fixed issue #123 - channel ids are now strings

--- packages/ui/src/components/TextMessages.vue
+++ packages/ui/src/components/TextMessages.vue
@@ -243,7 +243,7 @@ export default {
       );
     },
     currentJoinedChannel() {
-      return this.channelList.find((channel) => channel.cid === this.channelId);
+      return this.channelList.find((channel) => channel.cid == this.channelId);
     },
     textServerTab() {
       return {
